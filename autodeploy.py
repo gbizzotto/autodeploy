83a0d45dfc53e49e04a8f3331c439d793e8e1026
gabriel <gabriel.bizzotto@gmail.com>
2018-12-19 19:51:28 -0200

    Better logging.

--- autodeploy.py
+++ autodeploy.py
@@ -6,7 +6,10 @@ import subprocess, signal
 import git
 import datetime
 
+APP_NAME = 'autodeploy'
+
 def git_pull():
+    global APP_NAME
     # returns true if there are changes
     repo = git.Git()
     old_hash = repo.log().split('\n')[0]
@@ -15,7 +18,8 @@ def git_pull():
     new_hash = repo.log().split('\n')[0]
     has_pulled_something = new_hash != old_hash
     if has_pulled_something:
-        print str(datetime.datetime.now()), 'Upgrades...'
+        log_msg = repo.log().split('\n')[4]
+        print str(datetime.datetime.now()), '['+str(APP_NAME)+']', 'Upgrades...', log_msg
     return has_pulled_something
 
 def is_alive(pid):
@@ -28,30 +32,33 @@ def is_alive(pid):
         return True
 
 def term(pid):
-    print str(datetime.datetime.now()), 'term', pid
+    global APP_NAME
+    print str(datetime.datetime.now()), '['+str(APP_NAME)+']', 'term', pid
     os.kill(pid, signal.SIGTERM)
     for i in range(50):
         time.sleep(0.1)
         if not is_alive(pid):
             return
-    print str(datetime.datetime.now()), 'kill', pid
+    print str(datetime.datetime.now()), '['+str(APP_NAME)+']', 'kill', pid
     os.kill(pid, signal.SIGKILL)
 
 def start_program(program_and_args):
-    print str(datetime.datetime.now()), 'run', program_and_args
+    global APP_NAME
+    print str(datetime.datetime.now()), '['+str(APP_NAME)+']', 'run', program_and_args
     pid = subprocess.Popen(program_and_args, shell=False).pid
-    print str(datetime.datetime.now()), 'running with pid:', pid
+    print str(datetime.datetime.now()), '['+str(APP_NAME)+']', 'running with pid:', pid
     return pid
 
 def main():
+    global APP_NAME
     if len(sys.argv) <= 2:
         print 'Usage:'
         print '\tpython autodeploy.py <path> <program> [args...]'
         return
     path = sys.argv[1]
     program_and_args = sys.argv[2:]
-    print str(datetime.datetime.now()), 'entering path', path
-    print str(datetime.datetime.now()), 'running with', program_and_args
+    print str(datetime.datetime.now()), '['+str(APP_NAME)+']', 'entering path', path
+    print str(datetime.datetime.now()), '['+str(APP_NAME)+']', 'running with', program_and_args
     os.chdir(path)
     git_pull()
     pid = None  
@@ -67,12 +74,16 @@ def main():
                         if git_pull():
                             term(pid)
                             break
+                    except KeyboardInterrupt, e:
+                        raise e
                     except Exception, e:
-                        print str(datetime.datetime.now()), e
-                        print str(datetime.datetime.now()), traceback.format_exc()
+                        print str(datetime.datetime.now()), '['+str(APP_NAME)+']', e
+                        print str(datetime.datetime.now()), '['+str(APP_NAME)+']', traceback.format_exc()
+            except KeyboardInterrupt, e:
+                raise e
             except Exception, e:
-                print str(datetime.datetime.now()), e
-                print str(datetime.datetime.now()), traceback.format_exc()
+                print str(datetime.datetime.now()), '['+str(APP_NAME)+']', e
+                print str(datetime.datetime.now()), '['+str(APP_NAME)+']', traceback.format_exc()
                 time.sleep(1)
     except KeyboardInterrupt:
         term(pid)
